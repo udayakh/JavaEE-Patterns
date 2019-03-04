6e07cdc2c18a1f267209ce541d712611460a8861
Uday Kumar <udaykh4@gmail.com>
2019-03-04 17:51:55 +0000

    singletonPatterns

---
+++ SingletonPatterns/src/main/java/com/uday/patterns/Configuration.java
@@ -0,0 +1,15 @@
+package com.uday.patterns;
+
+import javax.ejb.Startup;
+import javax.inject.Singleton;
+
+/**
+ * Source code github.com/readlearncode
+ *
+ * @author Alex Theedom www.readlearncode.com
+ * @version 1.0
+ */
+@Startup
+@Singleton
+public class Configuration {
+}
